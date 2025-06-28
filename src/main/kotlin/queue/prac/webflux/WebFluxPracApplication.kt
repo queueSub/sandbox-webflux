59d95176a0e2820595ac462e662e0f9130a0f821
queueSub <kyusch0330@gmail.com>
2025-06-28 22:54:16 +0900

    Init project

---
+++ src/main/kotlin/queue/prac/webflux/WebFluxPracApplication.kt
@@ -0,0 +1,11 @@
+package queue.prac.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebFluxPracApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebFluxPracApplication>(*args)
+}
---
+++ src/test/kotlin/queue/prac/webflux/WebFluxPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package queue.prac.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebFluxPracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
